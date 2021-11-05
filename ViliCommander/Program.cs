dcf430bece84603ae1cd1fb1c6251b0e90bd3884
Valerij Medviď <vmedvid@gmail.com>
2021-11-05 22:20:43 +0100

    add DirectoryReaderService

--- ViliCommander/Program.cs
+++ ViliCommander/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace ViliCommander
 {
     internal class Program
     {
         static void Main(string[] args)
+
+
         {
-            Console.WriteLine("Hello World!");
+
+            DirectoryReaderService drs = new DirectoryReaderService(@"C:\");
+            foreach (string[] item in drs.getFoldersFromPath())
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(item[0].PadRight(30, ' ') + "| ");
+                Console.Write(item[1].PadLeft(8, ' ') + "| ");
+                Console.WriteLine(item[2].PadLeft(10, ' ') + "| ");
+
+            }
+            foreach (string[] item in drs.getFilesFromPath())
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(item[0].PadRight(30, ' ') + "| ");
+                Console.Write(item[1].PadLeft(8, ' ') + "| ");
+                Console.WriteLine(item[2].PadLeft(10, ' ') + "| ");
+
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ ViliCommander/Services/DirectoryReaderService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace ViliCommander
+{
+    public class DirectoryReaderService
+    {
+        string path;
+
+        public object ByteSize { get; private set; }
+
+        public DirectoryReaderService(string path = @"C:\")
+        {
+            this.path = path;
+        }
+
+
+        public List<string[]> getFoldersFromPath()
+        {
+            DirectoryInfo di = new DirectoryInfo(this.path);
+            DirectoryInfo[] dirs = di.GetDirectories();
+            List<string[]> result = new List<string[]>();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                string dirSize = formatFileSize(getDirSize(dir)).ToString();
+                string dirLastAccessTime = dir.LastAccessTime.ToShortDateString();
+
+                result.Add(new string[] { dir.Name, dirSize, dirLastAccessTime });
+            }
+
+            return result;
+        }
+
+
+        public List<string[]> getFilesFromPath()
+        {
+            string[] files = Directory.GetFiles(this.path);
+            List<string[]> result = new List<string[]>();
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string fileName = file.Replace(@"C:\", "").Split(@"\").Last();
+                string fileSize = formatFileSize(fileInfo.Length).ToString();
+                string fileLastModify = fileInfo.LastWriteTime.ToShortDateString();
+
+                result.Add(new string[] { fileName, fileSize, fileLastModify });
+            }
+
+            return result;
+        }
+
+
+        public static long getDirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            // Add file sizes.
+            FileInfo[] fis;
+            try
+            {
+                fis = d.GetFiles();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 0;
+            }
+
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += getDirSize(di);
+            }
+            return size;
+        }
+
+        public static string formatFileSize(long bytes)
+        {
+            var unit = 1024;
+            if (bytes < unit) { return $"{bytes} B"; }
+
+            var exp = (int)(Math.Log(bytes) / Math.Log(unit));
+            return $"{bytes / Math.Pow(unit, exp):F1} {("KMGTPE")[exp - 1]}B";
+        }
+    }
+}
