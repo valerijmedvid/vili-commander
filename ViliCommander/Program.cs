71d87ec3b153059866a86399ca4fd8b6e59afab1
Valerij Medviď <vmedvid@gmail.com>
2021-11-05 20:40:24 +0100

    initial commit

---
+++ ViliCommander/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ViliCommander
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
